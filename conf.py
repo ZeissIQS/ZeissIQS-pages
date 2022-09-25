c670e0aeafe492f44193d7833a16d697687aa287
Frank Blankenburg <fblankenburg@gom.com>
2022-09-25 22:02:34 +0200

    Reduced to IQS documentation landing page.

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Zeiss Industry Quality Solutions'
+project = 'ZEISS Industry Quality Solutioins'
 copyright = '2022, Carl Zeiss GOM Metrology GmbH'
 author = 'Carl Zeiss GOM Metrology GmbH'
 
@@ -15,14 +15,10 @@ author = 'Carl Zeiss GOM Metrology GmbH'
 
 extensions = ['myst_parser', 'sphinx_rtd_theme']
 source_suffix = ['.rst', '.md']
-
-templates_path = ['_templates']
 exclude_patterns = ['README.md']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
